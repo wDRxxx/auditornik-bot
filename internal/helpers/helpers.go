ec31e8e7c930d3be8866bbe86aa833c7bde89567
DRxxxxxxxxxx <ivancheb06@mail.ru>
2024-03-27 16:49:31 +0300

    error wrapping added

---
+++ internal/helpers/helpers.go
@@ -0,0 +1,26 @@
+package helpers
+
+import (
+	"fmt"
+	"log"
+	"runtime/debug"
+)
+
+// WrapErr оборачивает ошибку в text
+func WrapErr(text string, err error) error {
+	return fmt.Errorf("%s%w", text, err)
+}
+
+// WrapErrNotNil оборачивает ошибку в text если ошибка не nil
+func WrapErrNotNil(text string, err error) error {
+	if err == nil {
+		return nil
+	}
+	return fmt.Errorf("%s%w", text, err)
+}
+
+// ServerError выводит ошибку и ее стэк в консоль, а также возвращает обернутую ошибку
+func ServerError(text string, err error) error {
+	log.Printf("[ERR]: %s\n%s", err.Error(), debug.Stack())
+	return WrapErr(text, err)
+}
